3e1d0698375ed7ace3b74f72b4f880b1b841cf90
Jessica Flores Olmos <jessica.flores309@myhunter.cuny.edu>
2024-04-07 17:32:17 -0400

    CSS for homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,24 +6,28 @@ The Home component is used to demonstrate the use of Link.
 import React, {Component} from 'react';
 import AccountBalance from './AccountBalance';
 import {Link} from 'react-router-dom';
+import './Home.css';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <img src="https://picsum.photos/200/200" alt="bank"/>
+      <div className='homeBody'>
+        <div className='topHome'>
+          <img src="https://picsum.photos/200/200" alt="bank"/>
 
-        <h1>Bank of React</h1>
-
-        <Link to="/userProfile">User Profile</Link>
-        <br/>
-        <Link to="/login">Login</Link>
-        <br/>
-        <Link to="/credits">Credits (to be implemented in the Assignment)</Link>
-        <br/>
-        <Link to="/debits">Debits (to be implemented in the Assignment)</Link>
-        <br/><br/>
-        <AccountBalance accountBalance={this.props.accountBalance}/>
+          <h1 id='homeheader'>Bank of React</h1>
+        </div>
+        <div className='homeRedirects'>
+          <Link className="homeLink" to="/userProfile">User Profile</Link>
+          <br/>
+          <Link className="homeLink" to="/login">Login</Link>
+          <br/>
+          <Link className="homeLink" to="/credits">Credits</Link>
+          <br/>
+          <Link className="homeLink" to="/debits">Debits</Link>
+          <br/><br/>
+          <AccountBalance accountBalance={this.props.accountBalance}/>
+        </div>
       </div>
     );
   }
